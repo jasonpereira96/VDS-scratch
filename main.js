91b1f11ce6b3338ed9a75d4175cb0c429b172513
jasonpereira96 <jasonpereira074@gmail.com>
2022-10-15 20:36:09 -0500

    adding code

--- main.js
+++ main.js
@@ -1,71 +1,13 @@
 const width = 800, height = 600;
-const N = 10;
+const N = 12;
 const data = [];
 const side = 20;
 const E = side * 0.05
 const colors = ["pink", "aqua", "lightgreen"];
 let svg;
+let id = 0;
+let selectedRowIndex = null;
 
-function drag() {
-
-  function dragstarted(event, d) {
-    if (!d.isDragHandle) return;
-    d3.selectAll("." + getSquareClass(d)).raise().attr("stroke", "blue");
-  }
-
-  function dragged(event, d) {
-    if (!d.isDragHandle) return;
-    let s = d3.selectAll("." + getSquareClass(d)).attr("y", d.y = event.y)
-    d3.selectAll("." + getSquareClass(d)).filter(d => d.isDragHandle).attr("cy", d.y = event.y)
-
-    // console.log("dragged");
-    // console.log(s);
-    let datum = s.nodes()[0].__data__;
-    let y = datum.y;
-
-    let goldenZone = getGoldenZone(y);
-
-    if (between(goldenZone.upper.upperLimit, event.y, goldenZone.upper.lowerLimit)) {
-      console.log("swap");
-      let newData = data.slice();
-      // swapRows(newData, datum.row, datum.row - 1);
-      newData.forEach(d => {
-        if (d.row === datum.row - 1) { // upper row
-          // d.row++;
-          d.y += side;
-        }
-      })
-      renderMatrix(newData, svg);
-      goldenZone = getGoldenZone(y - side);
-    }
-
-    // .attr("transform", d => `translate(0, ${event.y / 2})`)
-  }
-
-  function dragended(event, d) {
-    if (!d.isDragHandle) return;
-    d3.selectAll("." + getSquareClass(d)).attr("stroke", "red");
-  }
-
-  return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-}
-
-function getGoldenZone(y) {
-  let goldenZone = {
-    upper: {
-      upperLimit: y - (side / 2) - E,
-      lowerLimit: y - (side / 2) + E,
-    },
-    lower: {
-      upperLimit: y + (side / 2) - E,
-      lowerLimit: y + (side / 2) + E,
-    }
-  };
-  return goldenZone;
-}
 
 for (let i=0; i<N; i++) {
   for (let j=0; j<N; j++) {
@@ -75,7 +17,8 @@ for (let i=0; i<N; i++) {
         y: i * side,
         row: i, col: j,
         color: "lightblue",
-        id: `${i}|${j}`,
+        selected: false,
+        id: id++,
         isDragHandle: true
       });
     } else {
@@ -84,48 +27,25 @@ for (let i=0; i<N; i++) {
         y: i * side,
         row: i, col: j,
         isDragHandle: false,
-        id: `${i}|${j}`,
+        id: id++,
         // color: colors[(i + j) % colors.length]
-        color: i % 2 === 0? "pink" : "lightblue"
-
+        color: getColor()
       });
     }
   }
 }
 
-/*
-const svg = d3.create("svg")
-  .attr("viewBox", [0, 0, width, height])
-  .attr("stroke-width", 2);
-
-  svg.selectAll("g")
-  .data(data)
-  .join("g")
-  // .attr("transform", d => `translate(0, ${d[0].y / 2})`)
-  // .attr("width", width)
-  // .attr("height", side)
-  // .attr("stroke", "black")
-  // .attr("fill", d => d.color)
-  // .attr("stroke-width", 0.3)
-  .attr("stroke", "red")
-  .attr("y", d => d[0].y)
-  .call(drag())
-  .selectAll("rect.square")
-  .data(d => d)
-  .join("rect")
-  .attr("x", d => d.x)
-  // .attr("y", d => d.y)
-  .attr("fill",d => d.color)
-  .attr("width", side)
-  .attr("height", side)
-  // .attr("stroke", "black")
-  */
 
   svg = d3.create("svg")
   .attr("viewBox", [0, 0, width, height])
   .attr("stroke-width", 2);
 
   renderMatrix(data, svg);
+
+  let handles = svg.selectAll(".drag-handle");
+  handles.on("click", onHandleClick);
+  
+    
   
 function renderMatrix (data, svg) { 
   svg.selectAll(".node")
@@ -135,25 +55,26 @@ function renderMatrix (data, svg) {
   .attr("stroke-width", 0.5)
   .attr("y", d => d.y)
   .attr("x", d => d.x)
-  .attr("class", d => getSquareClass(d))
+  .attr("class", d => getSquareClass(d) + " node")
   .attr("fill",d => d.color)
+  .attr("fill-opacity", d => d.selected ? 0.4 : 1)
   .attr("width", side)
   .attr("height", side)
 
-  svg.selectAll(".drag-handles")
-  .data(data.filter(d => d.isDragHandle))
+  return svg.selectAll(".drag-handle")
+  .data(data.filter(d => d.isDragHandle), d => d.id)
   .join("image")
-  .attr("stroke", "white")
   .attr("stroke-width", 0.5)
   .attr("y", d => d.y + 0 * side + 5)
   .attr("x", d => d.x + 0 * side + 5)
   // .attr("r", side / 2)
-  .attr("class", d => getSquareClass(d))
+  .attr("class", d => getSquareClass(d) + " drag-handle")
   // .attr("fill",d => d.color)
   .attr("width", side * 0.5)
   .attr("height", side * 0.5)
   .attr("href", "img/drag-handle.svg")
-  .call(drag())
+  // .on("click", onHandleClick);
+  
 }
 
 
@@ -187,6 +108,84 @@ function swapRows(data, rowIndex1, rowIndex2) {
     let t = d1.color;
     d1.color = d2.color;
     d2.color = t;
+
+    // let t = d1.y;
+    // d1.y = d2.y;
+    // d2.y = t;
+
+    // t = d1.row;
+    // d1.row = d2.row;
+    // d2.row = t;
+
+    // t = d1.selected;
+    // d1.selected = d2.selected;
+    // d2.selected = t;
   }
-  return data;
+}
+
+function randn_bm() {
+  let u = 0, v = 0;
+  while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while(v === 0) v = Math.random();
+  let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+  return num
+}
+
+function getColor() {
+  let randomNumber = Math.random();
+  return randomNumber <= 0.5 ? "pink" : "lightblue";
+}
+
+function onHandleClick(e, handleDatum) {
+
+  highlightRow(data, handleDatum.row);
+ 
+  renderMatrix(data, svg);
+  selectedRowIndex = handleDatum.row;
+}
+
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  if (selectedRowIndex) {
+    switch (e.key) {
+      case "ArrowUp" : {
+        if (selectedRowIndex === 0) return;
+        swapRows(data, selectedRowIndex, selectedRowIndex - 1);
+        highlightRow(data, selectedRowIndex - 1);
+        selectedRowIndex--;
+      } break;
+      case "ArrowDown" : {
+        if (selectedRowIndex === N-1) return;
+        swapRows(data, selectedRowIndex, selectedRowIndex + 1);
+        highlightRow(data, selectedRowIndex + 1);
+        selectedRowIndex++;
+      } break;
+      default: {}
+      break;
+    }
+  }
+  renderMatrix(data, svg);
+});
+
+document.getElementById("clear-button").addEventListener("click", () => {
+  clearSelection(data);
+})
+
+
+function highlightRow(data, rowIndex) {
+  data.forEach(d => {
+    if (d.row === rowIndex) {
+      d.selected = true;
+    } else {
+      d.selected = false;
+    }
+  });
+}
+
+function clearSelection(data) {
+  selectedRowIndex = null;
+  highlightRow(data, -1);
+  renderMatrix(data, svg);
 }
